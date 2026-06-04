00f46e8624dd251b84c2ee5048b648424dbc6c8f
mhmdzrkt34 <izaraket48@gmail.com>
2026-06-04 20:18:59 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,22 @@
+
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "API Running"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
