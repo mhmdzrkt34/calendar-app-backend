29cd60d10d27dffb7131239ec838d1beb2c25705
mhmdzrkt34 <izaraket48@gmail.com>
2026-06-09 21:11:28 +0300

    main >> modify server.js

--- src/server.js
+++ src/server.js
@@ -34,11 +34,6 @@ app.use((req, res, next) => {
     return res.status(200).json(res.locals.response);
 });
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 const startServer = async () => {
     try {
         await sequelize.authenticate();
